2227b0d787793e93d201e5eff43941c81d43fe9c
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-05 11:17:52 +0100

    first commit

---
+++ js_fundamentals.js
@@ -0,0 +1,25 @@
+var name = "TJ";
+// console.log("name:", name);
+
+var greeting = "Hello";
+// console.log("Greeting:", greeting);
+
+greeting = 5;
+// console.log("Greeting after change:", greeting);
+
+// console.log("hello" + " world");
+
+// console.log("hello" - " world");
+
+// console.log(`Hello ${name}`); 
+
+var myVariable;
+// console.log(myVariable);
+
+var myVariable = 5;
+// console.log(myVariable);
+
+console.log(3 + "hello");
+
+console.log('route' + 6 + 6);
+console.log(6 + 6 + 'route');
